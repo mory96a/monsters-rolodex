705505578b0a3e898a2e26fbfc6c3a52876ff3e8
mory aliaskari <mortezaasgari1996@gmail.com>
2020-09-25 20:39:20 +0330

    Updates

---
+++ precache-manifest.4daf05906b0f25ff6d1cb33f33b87361.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1bd80946fe7d18b519b21abfe0992f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e06fb387db47a3273787",
+    "url": "/monsters-rolodex/static/css/main.43085f86.chunk.css"
+  },
+  {
+    "revision": "17e7a3acfa6de3ea2ab4",
+    "url": "/monsters-rolodex/static/js/2.faf01414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.faf01414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06fb387db47a3273787",
+    "url": "/monsters-rolodex/static/js/main.4f17e2c1.chunk.js"
+  },
+  {
+    "revision": "893285d6c3aa078a52a1",
+    "url": "/monsters-rolodex/static/js/runtime-main.9af6a435.js"
+  }
+]);
